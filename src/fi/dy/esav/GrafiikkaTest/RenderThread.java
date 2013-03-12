929bb2dca06f8b7cce6ba198cbd591453349bca8
Esa Varemo <esa@kuivanto.fi>
2013-03-12 17:14:39 +0200

    Made a programmer-toggleable max_fps mode

--- src/fi/dy/esav/GrafiikkaTest/RenderThread.java
+++ src/fi/dy/esav/GrafiikkaTest/RenderThread.java
@@ -5,6 +5,8 @@ import android.view.View;
 
 public class RenderThread implements Runnable {
 
+    private static boolean MAX_FPS = false;
+
 	private BallView v;
 	boolean running = true;
 	boolean paused = false;
@@ -22,11 +24,13 @@ public class RenderThread implements Runnable {
 	public void run() {
 		while (running) {
 			v.draw();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+            if(!MAX_FPS) {
+			    try {
+				    Thread.sleep(5);
+			    } catch (InterruptedException e) {
+				    e.printStackTrace();
+			    }
+            }
 			while (paused && running) {
 				try {
 					Thread.sleep(50);
